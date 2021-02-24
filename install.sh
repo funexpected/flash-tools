7f2275c9f68bc7f91dcf90964b449598915b9f7b
Yakov Borevich <j.borevich@gmail.com>
2021-02-24 13:56:15 +0300

    fix install script

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ cd $TMPDIR
 curl -# -L https://github.com/funexpected/flash-tools/releases/latest/download/funexpected-tools.zip -o funexpected-tools.zip
 unzip -qq funexpected-tools.zip
 cd funexpected-tools/
-ls -d $HOME/Library/ApplicationSupport/Adobe/Animate*/*/Configuration/ | xargs -I{} cp -R ./* "{}"
+ls -d $HOME/Library/Application\ Support/Adobe/Animate*/*/Configuration | xargs -I{} cp -R ./* "{}"
 echo "Funexpected Flash Tools installed. Restart Adobe Animate and inspect 'Commands/Funexpected' menu."
