0ba87fbdf288b088750b06e5145839e893e1b9a5
Yakov Borevich <j.borevich@gmail.com>
2021-02-26 11:39:28 +0300

    fix toolkit path

--- install.sh
+++ install.sh
@@ -25,5 +25,6 @@ cd $TMPDIR
 curl -# -L https://github.com/funexpected/flash-tools/releases/latest/download/funexpected-tools.zip -o funexpected-tools.zip
 unzip -qq funexpected-tools.zip
 cd funexpected-tools/
+chmod a+x "Funexpected Tools/invoke"
 ls -d $HOME/Library/Application\ Support/Adobe/Animate*/*/Configuration/Commands/ | xargs -I{} cp -R ./* "{}"
 echo "Funexpected Flash Tools installed. Restart Adobe Animate and inspect 'Commands/Funexpected Tools' menu."
--- src/lib/tools.ts
+++ src/lib/tools.ts
@@ -186,7 +186,7 @@ export function cleanUpProject(path: String) {
 export function invoke(command: string, args:{ [id: string] : string; } = { }) {
     let isOSX = (fl.version.indexOf("MAC") != -1);
     let doc = fl.getDocumentDOM();
-    let toolkitPath = `${fl.configDirectory}Commands/Funexpected/toolkit`;
+    let toolkitPath = `${fl.configDirectory}Commands/Funexpected Tools/toolkit`;
     if (!isOSX) toolkitPath += ".exe";
     let cmd = `"${toolkitPath}" ${command}`;
     for (let arg in args) {
